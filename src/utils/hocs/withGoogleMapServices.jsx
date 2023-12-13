f4ed3ed9d925d46ec149287d9ce7ecaf2d1a416f
Choleski <louischoleski@gmail.com>
2023-12-13 18:37:48 -0500

    Update google map hoc.

--- src/utils/hocs/withGoogleMapServices.jsx
+++ src/utils/hocs/withGoogleMapServices.jsx
@@ -3,18 +3,19 @@ import { Status, Wrapper } from '@googlemaps/react-wrapper';
 
 import config from '@config';
 
-const DEFAULT_GOOGLE_MAP_API_KEY = config.services.googleMap;
-const DEFAULT_GOOGLE_MAP_SERVICES = ['places'];
+const GOOGLE_MAP_SERVICES = ['places'];
 
-const render = (status: Status) => {
-	if (status === Status.FAILURE) {
+const GOOGLE_MAP_API_KEY = config.services.googleMap;
+
+const render = (statusCode) => {
+	if (statusCode === Status.FAILURE) {
 		return <p>Failed</p>
 	}
 	return <p>loading...</p>
 }
 
 const withGoogleMapServices = (Component) => {
-	const WithGoogleMapServices = ({ apiKey = DEFAULT_GOOGLE_MAP_API_KEY, libraries = DEFAULT_GOOGLE_MAP_SERVICES, ...rest }) => (
+	const WithGoogleMapServices = ({ apiKey = GOOGLE_MAP_API_KEY, libraries = GOOGLE_MAP_SERVICES, ...rest }) => (
 		<Wrapper apiKey={apiKey} libraries={libraries} render={render}>
 			<Component {...rest} />
 		</Wrapper>
