9a146738aec925b44d15b2087efb38063f20cb23
Choleski <louischoleski@gmail.com>
2023-12-13 18:36:13 -0500

    Update user action.

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,4 +1,4 @@
-import * as authService from '@api/auth';
+import api from '@api';
 
 import * as actionTypes from './types';
 
@@ -8,7 +8,7 @@ export const login = ({ loginData }) => async (dispatch) => {
 		payload: { loading: true },
 	});
 
-	const data = await authService.login({ loginData });
+	const data = await api.auth.login({ loginData });
 
 	if (data.success === true) {
 		if (typeof window !== 'undefined') {
@@ -31,8 +31,37 @@ export const login = ({ loginData }) => async (dispatch) => {
 	}
 };
 
+export const register = ({ registerData }) => async (dispatch) => {
+	dispatch({
+		type: actionTypes.LOADING_REQUEST,
+		payload: { loading: true },
+	});
+
+	const data = await api.auth.register({ registerData });
+
+	if (data.success === true) {
+		if (typeof window !== 'undefined') {
+			window.localStorage.setItem('user', JSON.stringify(data.result));
+		}
+
+		dispatch({
+			type: actionTypes.REGISTER_SUCCESS,
+			payload: data.result,
+		});
+
+		return true;
+	} else {
+		dispatch({
+			type: actionTypes.FAILED_REQUEST,
+			payload: data,
+		});
+
+		return false;
+	}
+};
+
 export const logout = () => async (dispatch) => {
-	authService.logout();
+	api.auth.logout();
 
 	dispatch({
 		type: actionTypes.LOGOUT_SUCCESS,
