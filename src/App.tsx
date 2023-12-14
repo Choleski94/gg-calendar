33357de5e9704b458b6c10af0713ca98c1f8fca2
Choleski <louischoleski@gmail.com>
2023-12-14 13:35:24 -0500

    Add support for locale.

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { IntlProvider } from 'react-intl';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import locales from '@locales';
 import routes from '@constants/routes';
 import themes from '@components/themes';
 import { selectLocaleSettings } from '@store/selectors/settings';
+import { getLocale, constructLocale, SUPPORTED_LOCALES } from '@locales';
 
 const Tigado = () => {
-	const { lang } = useSelector(selectLocaleSettings);
+	const locale = useSelector(selectLocaleSettings);
 	const theme = 'light';
+
+	const localeISO = constructLocale(locale);
+	const localeMessage = getLocale(localeISO);
 
 	return (
 		<ThemeProvider theme={themes[theme]}>
-			<IntlProvider locale={lang} messages={locales[lang]} key={lang}>
+			<IntlProvider key={localeISO} locale={localeISO} messages={localeMessage}>
 				<Routes>
 					{(routes.map(({ slug, element: Component, path }) => (
 						<Route 
