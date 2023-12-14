02cfe9ebb776949b516b97cfd2d9211b9cb13f0b
Choleski <louischoleski@gmail.com>
2023-12-14 12:48:14 -0500

    Connect root app locale provider to store state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { IntlProvider } from 'react-intl';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
@@ -6,9 +7,10 @@ import { ThemeProvider } from 'styled-components';
 import locales from '@locales';
 import routes from '@constants/routes';
 import themes from '@components/themes';
+import { selectLocaleSettings } from '@store/selectors/settings';
 
 const Tigado = () => {
-	const lang = 'en-US';
+	const { lang } = useSelector(selectLocaleSettings);
 	const theme = 'light';
 
 	return (
