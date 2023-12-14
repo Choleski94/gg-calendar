acbdebc9345a8e5484c179f74986047ec67e6416
Choleski <louischoleski@gmail.com>
2023-12-14 12:04:44 -0500

    Add support for reload state init.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,35 @@
+import { jwtDecode } from 'jwt-decode';
 import { Suspense, lazy } from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
 
 import App from '@app';
-import store from '@store';
 import { Loader } from '@components';
+import rootReducer from '@store/rootReducer';
+import { userLoggedIn } from '@store/actions/auth';
+import setAuthorizationHeader from '@utils/setAuthorizationHeader';
 
 const root = document.getElementById('root');
+
+const store = configureStore({
+	reducer: rootReducer,
+	preloadedState: { user: {}, organizations: [], settings: {} },
+	devTools: import.meta.env.PROD === false, // Enable Redux DevTools in development mode
+});
+
+if (localStorage.tigadoJWT) {
+	const payload = jwtDecode(localStorage.tigadoJWT);
+
+	const user = {
+		token: localStorage.tigadoJWT,
+		...payload,
+	};
+
+	setAuthorizationHeader(localStorage.tigadoJWT);
+	store.dispatch(userLoggedIn(user));
+}
 
 if (root) {
 	ReactDOM.createRoot(root).render(
--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,3 @@ import rootReducer from './rootReducer';
 import storePersist, { localStorageHealthCheck } from './storePersist';
 
 localStorageHealthCheck();
-
-const user_state = storePersist.get('user') ? storePersist.get('user') : {};
-
-const initialState = { user: user_state };
-
-const store = configureStore({
-	reducer: rootReducer,
-	preloadedState: initialState,
-	devTools: import.meta.env.PROD === false, // Enable Redux DevTools in development mode
-});
-
-export default store;
