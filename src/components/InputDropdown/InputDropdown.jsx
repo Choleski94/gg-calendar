623c6b557911a545c5554721386a2f0dea0c5842
Choleski <louischoleski@gmail.com>
2024-01-01 14:57:52 -0500

    Update InputDropdown component.

--- src/components/InputDropdown/InputDropdown.jsx
+++ src/components/InputDropdown/InputDropdown.jsx
@@ -39,6 +39,8 @@ const InputDropdown = ({
 	const [ showDropdown, setShowDropdown ] = React.useState(false);
 
 	React.useEffect(() => {
+		if (!options || !options.length) return
+
 		const [ firstOption ] = options;
 		setActiveOption(defaultOption || firstOption.value);
 	}, []);
@@ -60,8 +62,8 @@ const InputDropdown = ({
 		onChange(data, activeOption);
 	}, [ data, activeOption ])
 
-	return (
-		<div className="input-group input-group-sm-vertical">
+	return(
+		<div className="input-group mb-2 mb-sm-0">
 			<Input
 				id={name}
 				type={type}
@@ -72,31 +74,36 @@ const InputDropdown = ({
 				required={required}
 				placeholder={placeholder}
 				onChange={handleInputChange}
-				className="js-input-mask form-control"
 			/>
-			<div className="tom-select-custom tom-select-custom-end">
-				<div className="ts-wrapper js-select form-select single plugin-change_listener plugin-hs_smart_position input-hidden full has-items dropdown-active">
-					<div className="ts-control" onClick={handleDropdownClick} style={{ minWidth: '110px', zIndex: 0 }}>
-						<div data-value="Mobile" className="item" data-ts-item>
-							{getOptionLabelByValue(options, activeOption)}
+			<div className="input-group-append input-group-append-last-sm-down-none">
+				<div className="tom-select-custom tom-select-custom-end">
+					<div className="ts-wrapper js-select form-select single plugin-change_listener plugin-hs_smart_position input-hidden full has-items">
+						<div className="ts-control" onClick={handleDropdownClick} style={{ minWidth: '110px', zIndex: 0 }}>
+							<div className="item" data-ts-item>
+								{getOptionLabelByValue(options, activeOption)}
+							</div>
 						</div>
-					</div>
-					<div className="tom-select-custom">
-						<div ref={dropdownRef} className={setDropwdownClassName(showDropdown)}>
-							<div className="ts-dropdown-content" role="listbox" tabIndex={-1}>
-								{options.map(({ value, label }, index) => (
-									<div key={value} 
-										data-selectable 
-										onClick={(e) => handleDropdownItemClick(e, value)}
-										className={setDropwdownItemClassName(activeOption, value)} 
-									>
-										{label}
-									</div>
-								))}
+						<div className="tom-select-custom">
+							<div ref={dropdownRef} className={setDropwdownClassName(showDropdown)}>
+								<div className="ts-dropdown-content" role="listbox" tabIndex={-1}>
+									{options.map(({ value, label }, index) => (
+										<div key={value} 
+											data-selectable 
+											onClick={(e) => handleDropdownItemClick(e, value)}
+											className={setDropwdownItemClassName(activeOption, value)} 
+										>
+											{label}
+										</div>
+									))}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<a className="btn btn-primary d-none d-sm-inline-block" href="javascript:;">
+					<i className="bi bi-plus"/>
+					Invite
+				</a>
 			</div>
 		</div>
 	);
