a305b5fd2680625673664d328abb9f681f0ff287
Choleski <louischoleski@gmail.com>
2024-01-04 14:53:40 -0500

    Add proper support for table footet cta.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -207,10 +207,6 @@ const Table = ({
 		cta
 	), []);
 
-	const showFooter = React.useMemo(() => Boolean(
-		footerCta
-	), []);
-
 	const hasActiveItems = React.useMemo(() => (
 		Boolean(activeItems && activeItems?.length)
 	), [ activeItems ]);
@@ -384,75 +380,87 @@ const Table = ({
 			)}
 
 			{activeItems && activeItems?.length ? (
-				<div className="table-responsive datatable-custom position-relative">
-					<div id="datatable_wrapper" className="dataTables_wrapper no-footer">
-						<table role="grid" className={getTableClassName(Boolean(onRowClick))}>
-							{headers && headers.length && !withoutHeader ? (
-								<thead className="thead-light">
-									<tr role="row">
-										{headers.filter(({ key }) => activeKeys.includes(key)).map(({ key, label }, colIdx = 0) => (
-											<th
-												key={`header-${key}`} rowSpan="1" colSpan="1" onClick={(e) => onFilter(e, key)}
-												className={[
-													// (colIdx > 0 ? 'sorting' : ''), 
-													(colIdx > 0 ? 'table-column-ps-0' : '')
-												].join(' ')}
-											>
-												{label}
-												{label && label.length && withFilter && (
-													<i className={getClassName(getClassNamesFor(key, sortConfig))} />
-												)}
-											</th>
-										))}
-									</tr>
-								</thead>
-							) : null}
-							<tbody>
-								{loading ? (
-									<tr className="odd">
-										<td valign="top" colSpan="12" className="dataTables_empty">
-											<div className="text-center p-4">
-												<div
-													role="status"
-													className="spinner-border mt-3 mb-5"
-													style={{ width: "3rem", height: "3rem" }}
+				<>
+					<div className="table-responsive datatable-custom position-relative">
+						<div id="datatable_wrapper" className="dataTables_wrapper no-footer">
+							<table role="grid" className={getTableClassName(Boolean(onRowClick))}>
+								{headers && headers.length && !withoutHeader ? (
+									<thead className="thead-light">
+										<tr role="row">
+											{headers.filter(({ key }) => activeKeys.includes(key)).map(({ key, label }, colIdx = 0) => (
+												<th
+													key={`header-${key}`} rowSpan="1" colSpan="1" onClick={(e) => onFilter(e, key)}
+													className={[
+														// (colIdx > 0 ? 'sorting' : ''), 
+														(colIdx > 0 ? 'table-column-ps-0' : '')
+													].join(' ')}
 												>
-													<span className="visually-hidden">
-														Loading...
-													</span>
+													{label}
+													{label && label.length && withFilter && (
+														<i className={getClassName(getClassNamesFor(key, sortConfig))} />
+													)}
+												</th>
+											))}
+										</tr>
+									</thead>
+								) : null}
+								<tbody>
+									{loading ? (
+										<tr className="odd">
+											<td valign="top" colSpan="12" className="dataTables_empty">
+												<div className="text-center p-4">
+													<div
+														role="status"
+														className="spinner-border mt-3 mb-5"
+														style={{ width: "3rem", height: "3rem" }}
+													>
+														<span className="visually-hidden">
+															Loading...
+														</span>
+													</div>
+													<p className="mb-0">
+														{loadingDataMessage}
+													</p>
 												</div>
-												<p className="mb-0">
-													{loadingDataMessage}
-												</p>
-											</div>
-										</td>
-									</tr>
-								) : (
-									activeItems.map((item = {}, jobIdx = 0) => {
-										const activeId = item.key || genId();
-										return (
-											<tr role="row" className={`${isEven(jobIdx) ? 'even' : 'odd'} ${(jobIdx === defaultIdx) ? ('active') : ('')}`} 
-											onClick={e => toggleQuickView(e, activeId, item)}>
-												{headerKeys.filter((currentKey) => activeKeys.includes(currentKey)).map((key = '', colIdx = 0) => (
-													<td key={`td-${key}-${activeId}`} className={`${colIdx > 0 ? 'table-column-ps-0' : ''}`}>
-														{item[key]}
-													</td>
-												))}
-											</tr>
-										);
-									})
-								)}
-							</tbody>
-						</table>
-						<div
-							role="status"
-							aria-live="polite"
-							className="dataTables_info"
-						>
-							Showing 1 to 15 of {(data || []).length} entries
+											</td>
+										</tr>
+									) : (
+										activeItems.map((item = {}, jobIdx = 0) => {
+											const activeId = item.key || genId();
+											return (
+												<tr role="row" className={`${isEven(jobIdx) ? 'even' : 'odd'} ${(jobIdx === defaultIdx) ? ('active') : ('')}`} 
+												onClick={e => toggleQuickView(e, activeId, item)}>
+													{headerKeys.filter((currentKey) => activeKeys.includes(currentKey)).map((key = '', colIdx = 0) => (
+														<td key={`td-${key}-${activeId}`} className={`${colIdx > 0 ? 'table-column-ps-0' : ''}`}>
+															{item[key]}
+														</td>
+													))}
+												</tr>
+											);
+										})
+									)}
+								</tbody>
+							</table>
+							<div
+								role="status"
+								aria-live="polite"
+								className="dataTables_info"
+							>
+								Showing 1 to 15 of {(data || []).length} entries
+							</div>
 						</div>
 					</div>
-				</div>
+					{footerCta ? (
+						<div className="row justify-content-center justify-content-sm-between align-items-sm-center">
+							<div className="col-sm mb-2 mb-sm-0" />
+							<div className="col-sm-auto m-4">
+								<div className="d-flex justify-content-center justify-content-sm-end">
+									{footerCta}
+								</div>
+							</div>
+						</div>
+					) : null}
+				</>
 			) : (
 				<Card.Body fullHeight>
 					{loading ? (
@@ -485,18 +493,6 @@ const Table = ({
 				</Card.Body>
 			)}
 
-			{showFooter ? (
-				<Card.Footer>
-					<div className="row justify-content-center justify-content-sm-between align-items-sm-center">
-						<div className="col-sm mb-2 mb-sm-0" />
-						<div className="col-sm-auto">
-							<div className="d-flex justify-content-center justify-content-sm-end">
-								{footerCta}
-							</div>
-						</div>
-					</div>
-				</Card.Footer>
-			) : null}
 		</Card>
 	);
 };
