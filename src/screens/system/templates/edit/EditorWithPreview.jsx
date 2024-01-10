babc421aa68a7e63e6f71db2631ff8d6edf0344b
Choleski <louischoleski@gmail.com>
2024-01-10 15:45:12 -0500

    Add support for template layout.

--- src/screens/system/templates/edit/EditorWithPreview.jsx
+++ src/screens/system/templates/edit/EditorWithPreview.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Handlebars from 'handlebars';
 import MonacoEditor from 'react-monaco-editor';
 
+import compileTemplate from '@utils/compileTemplate';
+
 import { Card, Layout, NavPill, Breadcrumb, GetSupport } from '@components';
 
 const BREADCRUMB_OPTIONS = [
@@ -37,7 +39,7 @@ const NAV_TAB_OPTIONS = [
 ];
 
 // Define the initial Handlebars template
-const initialTemplate = 
+const initialLayout = 
 `<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 	<head>
 		<title> </title>
@@ -65,167 +67,184 @@ const initialTemplate =
 	<body style="background-color:#f9f9f9;">
 		<div style="background-color:#f9f9f9;">
 			<!--[if mso | IE]> 
-			<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
-				<tr>
-					<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-						<![endif]--> 
-						<div style="background:#f9f9f9;background-color:#f9f9f9;Margin:0px auto;max-width:600px;">
-							<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#f9f9f9;background-color:#f9f9f9;width:100%;">
-								<tbody>
-									<tr>
-										<td style="border-bottom:#333957 solid 5px;direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-											<!--[if mso | IE]> 
-											<table role="presentation" border="0" cellpadding="0" cellspacing="0">
-												<tr> </tr>
-											</table>
-											<![endif]--> 
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<!--[if mso | IE]> 
-					</td>
-				</tr>
-			</table>
-			<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
-				<tr>
-					<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-						<![endif]--> 
-						<div style="background:#fff;background-color:#fff;Margin:0px auto;max-width:600px;">
-							<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#fff;background-color:#fff;width:100%;">
-								<tbody>
-									<tr>
-										<td style="border:#dddddd solid 1px;border-top:0px;direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-											<!--[if mso | IE]> 
-											<table role="presentation" border="0" cellpadding="0" cellspacing="0">
-												<tr>
-													<td style="vertical-align:bottom;width:600px;">
-														<![endif]--> 
-														<div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:bottom;width:100%;">
-															<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:bottom;" width="100%">
-																<tbody>
-																	<tr>
-																		<td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-																			<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
-																				<tbody>
-																					<tr>
-																						<td style="width:64px;"> <img height="auto" src="https://i.imgur.com/KO1vcE9.png" style="border:0;display:block;outline:none;text-decoration:none;width:100%;" width="64"> </td>
-																					</tr>
-																				</tbody>
-																			</table>
-																		</td>
-																	</tr>
-																	<tr>
-																		<td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:40px;word-break:break-word;">
-																			<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:28px;font-weight:bold;line-height:1;text-align:center;color:#555;"> Welcome to {{company}}</div>
-																		</td>
-																	</tr>
-																	<tr>
-																		<td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-																			<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:16px;line-height:22px;text-align:left;color:#555;"> Hello {{client}}!<br/><br/>Thank you for signing up for {{company}}. We are thrilled to have you as a member of our {{company}} family. With your new account, you will have access to exclusive content, special offers, and much more!<br/><br/>To get started, please click the link below to verify your account and set up your login credentials: </div>
-																		</td>
-																	</tr>
-																	<tr>
-																		<td align="center" style="font-size:0px;padding:10px 25px;padding-top:30px;padding-bottom:50px;word-break:break-word;">
-																			<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
-																				<tbody>
-																					<tr>
-																						<td align="center" bgcolor="#2F67F6" role="presentation" style="border:none;border-radius:3px;color:#ffffff;cursor:auto;padding:15px 25px;" valign="middle"> <a style="background:#2F67F6;color:#ffffff;font-family:'Helvetica Neue',Arial,sans-serif;font-size:15px;font-weight:normal;line-height:120%;Margin:0;text-decoration:none;text-transform:none;" href=""> Activate Your Account</a> </td>
-																					</tr>
-																				</tbody>
-																			</table>
-																		</td>
-																	</tr>
-																	<tr>
-																		<td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
-																			<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:20px;text-align:left;color:#525252;"> Best regards,<br/><br/>The {{company}} Team<br/> </div>
-																		</td>
-																	</tr>
-																</tbody>
-															</table>
-														</div>
-														<!--[if mso | IE]> 
-													</td>
-												</tr>
-											</table>
-											<![endif]--> 
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<!--[if mso | IE]> 
-					</td>
-				</tr>
-			</table>
-			<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
-				<tr>
-					<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
-						<![endif]--> 
-						<div style="Margin:0px auto;max-width:600px;">
-							<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
-								<tbody>
-									<tr>
-										<td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
-											<!--[if mso | IE]> 
-											<table role="presentation" border="0" cellpadding="0" cellspacing="0">
-												<tr>
-													<td style="vertical-align:bottom;width:600px;">
-														<![endif]--> 
-														<div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:bottom;width:100%;">
-															<table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
-																<tbody>
-																	<tr>
-																		<td align="center" style="font-size:0px;padding:0;word-break:break-word;">
-																			<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:12px;font-weight:300;line-height:1;text-align:center;color:#575757;"> Copyright &copy; {{year}} {{comapny}}</div>
-																		</td>
-																	</tr>
-																	<tr>
-																		<td align="center" style="font-size:0px;padding:10px;word-break:break-word;">
-																			<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:12px;font-weight:300;line-height:1;text-align:center;color:#575757;"> <a href="/unsubscribe" style="color:#575757">Unsubscribe</a> from our emails </div>
-																		</td>
-																	</tr>
-																</tbody>
-															</table>
-														</div>
-														<!--[if mso | IE]> 
-													</td>
-												</tr>
-											</table>
-											<![endif]--> 
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<!--[if mso | IE]> 
-					</td>
-				</tr>
-			</table>
+			{{> body }}
 			<![endif]--> 
 		</div>
 	</body>
 </html>
 `;
 
+const contentPartial = 
+`
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
+	<tr>
+		<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+			<![endif]--> 
+			<div style="background:#f9f9f9;background-color:#f9f9f9;Margin:0px auto;max-width:600px;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#f9f9f9;background-color:#f9f9f9;width:100%;">
+					<tbody>
+						<tr>
+							<td style="border-bottom:#333957 solid 5px;direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
+								<!--[if mso | IE]> 
+								<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+									<tr> </tr>
+								</table>
+								<![endif]--> 
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!--[if mso | IE]> 
+		</td>
+	</tr>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
+	<tr>
+		<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+			<![endif]--> 
+			<div style="background:#fff;background-color:#fff;Margin:0px auto;max-width:600px;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#fff;background-color:#fff;width:100%;">
+					<tbody>
+						<tr>
+							<td style="border:#dddddd solid 1px;border-top:0px;direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
+								<!--[if mso | IE]> 
+								<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+									<tr>
+										<td style="vertical-align:bottom;width:600px;">
+											<![endif]--> 
+											<div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:bottom;width:100%;">
+												<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:bottom;" width="100%">
+													<tbody>
+														<tr>
+															<td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+																<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+																	<tbody>
+																		<tr>
+																			<td style="width:64px;"> <img height="auto" src="https://i.imgur.com/KO1vcE9.png" style="border:0;display:block;outline:none;text-decoration:none;width:100%;" width="64"> </td>
+																		</tr>
+																	</tbody>
+																</table>
+															</td>
+														</tr>
+														<tr>
+															<td align="center" style="font-size:0px;padding:10px 25px;padding-bottom:40px;word-break:break-word;">
+																<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:28px;font-weight:bold;line-height:1;text-align:center;color:#555;"> Welcome to {{company}}</div>
+															</td>
+														</tr>
+														<tr>
+															<td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+																<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:16px;line-height:22px;text-align:left;color:#555;"> Hello {{client}}!<br/><br/>Thank you for signing up for {{company}}. We are thrilled to have you as a member of our {{company}} family. With your new account, you will have access to exclusive content, special offers, and much more!<br/><br/>To get started, please click the link below to verify your account and set up your login credentials: </div>
+															</td>
+														</tr>
+														<tr>
+															<td align="center" style="font-size:0px;padding:10px 25px;padding-top:30px;padding-bottom:50px;word-break:break-word;">
+																<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
+																	<tbody>
+																		<tr>
+																			<td align="center" bgcolor="#2F67F6" role="presentation" style="border:none;border-radius:3px;color:#ffffff;cursor:auto;padding:15px 25px;" valign="middle"> <a style="background:#2F67F6;color:#ffffff;font-family:'Helvetica Neue',Arial,sans-serif;font-size:15px;font-weight:normal;line-height:120%;Margin:0;text-decoration:none;text-transform:none;" href=""> Activate Your Account</a> </td>
+																		</tr>
+																	</tbody>
+																</table>
+															</td>
+														</tr>
+														<tr>
+															<td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+																<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:14px;line-height:20px;text-align:left;color:#525252;"> Best regards,<br/><br/>The {{company}} Team<br/> </div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+											<!--[if mso | IE]> 
+										</td>
+									</tr>
+								</table>
+								<![endif]--> 
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!--[if mso | IE]> 
+		</td>
+	</tr>
+</table>
+<table align="center" border="0" cellpadding="0" cellspacing="0" style="width:600px;" width="600">
+	<tr>
+		<td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+			<![endif]--> 
+			<div style="Margin:0px auto;max-width:600px;">
+				<table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+					<tbody>
+						<tr>
+							<td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
+								<!--[if mso | IE]> 
+								<table role="presentation" border="0" cellpadding="0" cellspacing="0">
+									<tr>
+										<td style="vertical-align:bottom;width:600px;">
+											<![endif]--> 
+											<div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:bottom;width:100%;">
+												<table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+													<tbody>
+														<tr>
+															<td align="center" style="font-size:0px;padding:0;word-break:break-word;">
+																<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:12px;font-weight:300;line-height:1;text-align:center;color:#575757;"> Copyright &copy; {{getCurrentYear}} {{comapny}}</div>
+															</td>
+														</tr>
+														<tr>
+															<td align="center" style="font-size:0px;padding:10px;word-break:break-word;">
+																<div style="font-family:'Helvetica Neue',Arial,sans-serif;font-size:12px;font-weight:300;line-height:1;text-align:center;color:#575757;"> <a href="/unsubscribe" style="color:#575757">Unsubscribe</a> from our emails </div>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+											<!--[if mso | IE]> 
+										</td>
+									</tr>
+								</table>
+								<![endif]--> 
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!--[if mso | IE]> 
+		</td>
+	</tr>
+</table>
+`;
+
 const EditorWithPreview = ({ setTemplateId }) => {
 	// State to manage the template content and variables
-	const [ template, setTemplate ] = React.useState(initialTemplate);
+	const [ content, setContent ] = React.useState('');
+	const [ previewHTML, setPreviewHTML ] = React.useState('');
 	const [ variables, setVariables ] = React.useState({
-		year: '2024',
 		client: 'Julian Guzman',
 		company: 'Flash Repair',
 		content: 'This is a demo with variable support.',
 	});
 
-	// Compile the template with Handlebars
-	const compiledTemplate = Handlebars.compile(template);
-	const previewHTML = compiledTemplate(variables);
+	React.useEffect(() => {
+		setContent(contentPartial);
+	}, []);
+
+	React.useEffect(() => {
+		// Compile the template with Handlebars
+		const template = compileTemplate({
+			partials: {
+				description: content,
+			},
+			variables,
+		});
+
+		setPreviewHTML(template);
+	}, [ content, variables ]);
 
 	// Handle editor content change
 	const handleEditorChange = (value, event) => {
-		setTemplate(value);
+		setContent(value);
 	};
 
 	return (
@@ -276,10 +295,10 @@ const EditorWithPreview = ({ setTemplateId }) => {
 							<div className="col-md-7">
 								<div style={{ width: '100%', height: '100%' }}>
 									<MonacoEditor
+										theme="light"
 										height="100%"
 										language="html"
-										theme="light"
-										value={template}
+										value={content}
 										options={{
 											minimap: {
 												enabled: false,
