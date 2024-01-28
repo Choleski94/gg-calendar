c84cb2141dba57d82d27809b145bc421e04a950c
Choleski <louischoleski@gmail.com>
2024-01-28 10:12:25 -0500

    Update screens.

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Authentication
+// Auth
 export { default as SignIn } from './auth/Signin';
 export { default as SignUp } from './auth/Signup';
 // export { default as ForgotPassword } from './forgot_password';
 
-// Account
-// export { default as AccountMember } from './account/member';
+// App
+export { default as Home } from './home';
 // export { default as AccountProfile } from './account/profile';
 // export { default as AccountSettings } from './account/settings';
 // export { default as AccountAccessLogs } from './account/access-logs';
