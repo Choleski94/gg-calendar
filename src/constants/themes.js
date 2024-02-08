d216fedba087b13d97c01be758e67840bc265e35
Choleski <louischoleski@gmail.com>
2024-02-08 02:25:41 -0500

    Add theme constants.

---
+++ src/constants/themes.js
@@ -0,0 +1,16 @@
+export const THEME_KEYS = {
+	DARK: 'DARK',
+	LIGHT: 'LIGHT',
+	CONTRAST: 'CONTRAST',
+}
+
+export const THEME_CLASSNAMES = {
+	DARK: 'body',
+	LIGHT: 'body light-mode',
+	CONTRAST: 'body contrast-mode',
+}
+
+export default {
+	THEME_KEYS,
+	THEME_CLASSNAMES,
+}
