03729bc206ee63a412a3bbd712ccc333f00c8167
Choleski <louischoleski@gmail.com>
2024-02-12 11:08:04 -0500

    Remove support for react-router-dom

--- src/utils/hocs/index.js
+++ src/utils/hocs/index.js
@@ -1,5 +1,3 @@
-export { default as withGuestRouter } from './withGuestRouter';
 export { default as withScrollToTop } from './withScrollToTop';
-export { default as withPrivateRouter } from './withPrivateRouter';
 export { default as withBrowserDetect } from './withBrowserDetect';
 export { default as withShortcutsListener } from './withShortcutsListener';
