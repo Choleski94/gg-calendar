7db34b04fb1667fb410a302c0371ea48461d9fa0
Choleski <louischoleski@gmail.com>
2024-02-14 10:42:38 -0500

    Deprecate utlity helper functions, update utility functions.

--- src/utils/hocs/index.js
+++ src/utils/hocs/index.js
@@ -1,3 +1,2 @@
 export { default as withScrollToTop } from './withScrollToTop';
-export { default as withBrowserDetect } from './withBrowserDetect';
 export { default as withShortcutsListener } from './withShortcutsListener';
