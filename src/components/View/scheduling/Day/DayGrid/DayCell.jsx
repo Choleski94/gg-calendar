9f7527d6740383e31f070e70be2c42316e62e0ff
Choleski <louischoleski@gmail.com>
2024-02-14 14:22:00 -0500

    Refactor DayCell component.

--- src/components/View/scheduling/Day/DayGrid/DayCell.jsx
+++ src/components/View/scheduling/Day/DayGrid/DayCell.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 import { calcTime } from '@utils/time';
 
-const createBoxStyle = (coord) => ({
-	top: `${+coord.y * 12.5}px`,
-	height: `${+coord.h * 12.5}px`,
-	width: 'calc((100% - 4px) * 1)',
-	left: 'calc((100% - 0px) * 0 + 0px)',
-});
+import { setDayCellStyle } from './DayCell.controller';
 
 const DayCell = ({ id, category, title, coordinates }) => {
 	return (
@@ -19,10 +14,7 @@ const DayCell = ({ id, category, title, coordinates }) => {
 			data-dv-end-time={coordinates.e}
 			data-dv-start-time={coordinates.y}
 			data-dv-time-intervals={coordinates.h}
-			style={{
-				...createBoxStyle(coordinates),
-				backgroundColor: "rgb(44, 82, 186)",
-			}}
+			style={setDayCellStyle(coordinates, "rgb(44, 82, 186)")}
 		>
 			<div className="dv-box__header">
 				<div className="dv-box-title">
