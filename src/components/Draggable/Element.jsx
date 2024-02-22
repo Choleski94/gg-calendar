8072720ad3fc6795a2735fd49b4b43f4b74ece3d
Choleski <louischoleski@gmail.com>
2024-02-22 08:43:22 -0500

    Move dv-box className to element class name 7 add proper support for box cloning.

--- src/components/Draggable/Element.jsx
+++ src/components/Draggable/Element.jsx
@@ -18,25 +18,31 @@ const Element = ({
 	lockY = false,
 	children = null, 
 	x = 0,  y = 0, h = 0, 
-	// handleDrag = () => null,
-	// handleResize = () => null,
-	...rest
 }) => {
 	const [ height, setHeight ] = React.useState(0);
+	const positionRef = React.useRef({ x: 0, y: 0 }); // Use a ref to store the position
 	const [ isResizing, setIsResizing ] = React.useState(false);
 	const [ isDragging, setIsDragging ] = React.useState(false);
 	const [ position, setPosition ] = React.useState({ x: 0, y: 0 });
+	const [ prevPosition, setPrevPosition ] = React.useState({ x: 0, y: 0 });
 
 	const { data: { parentOffsetTop, parentScrollTop, parentBoundingClientRect }, updateData } = useContext();
 
+	const headerOffset = parseInt(parentOffsetTop);
+	const amountScrolled = parseInt(parentScrollTop);
+
+	// Initialize.
 	React.useEffect(() => {
 		setHeight(h);
 		setPosition({ x, y });
-		console.log('Height:::', h, height, x, y);
+		setPrevPosition({ x, y });
+		positionRef.current = { x, y };
 	}, [ h, x, y ]);
 
-	const headerOffset = parseInt(parentOffsetTop);
-	const amountScrolled = parseInt(parentScrollTop);
+	const updatePosition = ({ x, y }) => {
+		setPosition({ x, y });
+		positionRef.current = { x, y };
+	}
 
 	const handleMouseUp = (e) => {
 		e.preventDefault();
@@ -58,128 +64,116 @@ const Element = ({
 		let [ sX, sY ] = [ 0, 0 ];
 
 		/** DRAG NORTH SOUTH */
-		const handleMouseMove = (e) => {
-			sX = Math.abs(e.clientX - tempX);
-			sY = Math.abs(e.clientY - tempY);
+		const _handleMouseMove = (e) => {
+			if (isResizing) {
+				const box = e.target.parentElement;
+				const boxTop = box.offsetTop;
 
-			const currentCursorY = e.pageY - headerOffset;
-			const newOffsetY = currentCursorY - startCursorY;
+				// TODO
+				const newHeight = Math.round((
+					e.pageY + 
+					amountScrolled - 
+					boxTop - 
+					headerOffset
+				) / 12.5) * 12.5;
 
-			let newTop = Math.round((newOffsetY + startTop) / 12.5);
+				if (newHeight <= 12.5) {
+					console.log('Situation A');
+					return;
+				} else if (newHeight + boxTop > 1188) {
+					console.log('Situation B');
+					return;
+				} else {
+					// console.log('Situation C');
+					setHeight(newHeight);
+				}
 
-			// if (newTop < 0 || currentCursorY < 0) {
-			// 	newTop = 0;
-			// 	return;
-			// } else if (newTop + boxHeight > 1188) {
-			// 	return;
-			// }
+				// const deltaY = e.clientY - startY;
 
-			setPosition({
-				x: lockX ? position.x : position.x + deltaX,
-				y: newTop,
-			});
+				// // setPosition((prevPosition) => ({
+				// // 	x: prevPosition.x,
+				// // 	y: lockY ? prevPosition.y : prevPosition.y + deltaY,
+				// // }));
 
-			// TODO: Refactor
-			// const hasSibling = false;
-			// const box = e.target.parentElement;
+				// if (handleResize) {
+				// 	console.log('Resize.....', id, deltaY)
+				// 	handleResize(id, deltaY);
+				// }
+			} else {
+				sX = Math.abs(e.clientX - tempX);
+				sY = Math.abs(e.clientY - tempY);
 
-			// const boxTop = box.offsetTop;
-			// const col = box.parentElement;
+				const currentCursorY = e.pageY - headerOffset;
+				const newOffsetY = currentCursorY - startCursorY;
 
-			// console.log('Mouse move....', isResizing);
+				let newTop = Math.round((newOffsetY + startTop) / 12.5);
 
-			// if (isResizing) {
-			// 	// // Clone box
-			// 	// const clone = box.cloneNode(true);
-			// 	// // clone.classList.add(`${identifiers.boxClasses[view].temporary}`);
-			// 	// clone.classList.add('temporary-box');
+				// if (newTop < 0 || currentCursorY < 0) {
+				// 	newTop = 0;
+				// 	return;
+				// } else if (newTop + boxHeight > 1188) {
+				// 	return;
+				// }
 
-			// 	// if (hasSibling) {
-			// 	// 	col.insertBefore(clone, box.nextElementSibling);
-			// 	// } else {
-			// 	// 	col.appendChild(clone);
-			// 	// }
-
-			// 	// TODO
-			// 	const newHeight = Math.round((
-			// 		e.pageY + 
-			// 		amountScrolled - 
-			// 		boxTop - 
-			// 		headerOffset
-			// 	) / 12.5) * 12.5;
-
-			// 	if (newHeight <= 12.5) {
-			// 		console.log('Situation A');
-			// 		return;
-			// 	} else if (newHeight + boxTop > 1188) {
-			// 		console.log('Situation B');
-			// 		return;
-			// 	} else {
-			// 		// console.log('Situation C');
-			// 		setHeight(newHeight);
-			// 	}
-
-			// 	// const deltaY = e.clientY - startY;
-
-			// 	// // setPosition((prevPosition) => ({
-			// 	// // 	x: prevPosition.x,
-			// 	// // 	y: lockY ? prevPosition.y : prevPosition.y + deltaY,
-			// 	// // }));
-
-			// 	// if (handleResize) {
-			// 	// 	console.log('Resize.....', id, deltaY)
-			// 	// 	handleResize(id, deltaY);
-			// 	// }
-			// } else {
-			// 	const [ deltaX, deltaY ] = [
-			// 		e.clientX - startX, 
-			// 		e.clientY - startY
-			// 	];
-
-			// 	setPosition({
-			// 		x: lockX ? position.x : position.x + deltaX,
-			// 		y: lockY ? position.y : position.y + deltaY,
-			// 	});
-
-			// 	// if (handleDrag) {
-			// 	// 	handleDrag(id, position.x + deltaX, position.y + deltaY);
-			// 	// }
-			// }
+				updatePosition({
+					x: lockX ? position.x : position.x + deltaX,
+					y: newTop,
+				});
+			}
 		};
 
-		const handleMouseUp = () => {
+		const _handleMouseUp = () => {
 			console.log('Handle resize mouse up....');
+
 			setIsResizing(false);
 			setIsDragging(false);
 
-			document.removeEventListener('mousemove', handleMouseMove);
-			document.removeEventListener('mouseup', handleMouseUp);
+			// Save the previous position.
+			setPrevPosition(positionRef.current);
+
+			document.removeEventListener('mousemove', _handleMouseMove);
+			document.removeEventListener('mouseup', _handleMouseUp);
 		};
 
-		document.addEventListener('mousemove', handleMouseMove);
-		document.addEventListener('mouseup', handleMouseUp);
+		document.addEventListener('mousemove', _handleMouseMove);
+		document.addEventListener('mouseup', _handleMouseUp);
 	};
 
 	const handleResizeMouseDown = (e) => {
-		// e.stopPropagation();
+		e.stopPropagation();
 		console.log('Handle resize mouse down....');
 		setIsResizing(true);
 	};
 
-	const elementStyle = React.useMemo(() => (
-		setElementStyle(position, height, isDragging, isResizing)
-	), [ position, isDragging, isResizing ]);
+	// Clone element.
+	const cloneElement = React.cloneElement(children, {
+		className: [
+			children.props.className, 'dv-box',
+			isDragging ? 'dv-box-dragging' : null,
+			isResizing ? 'dv-box-resizing' : null,
+			'dv-temporary-box'
+		].join(' ').trim(),
+		style: {
+			...children.props.style,
+			...setElementStyle(prevPosition, height, isDragging, isResizing),
+		},
+		children: [
+			children.props.children,
+		],
+	});
 
-	return React.cloneElement(children, {
+	// Set main element.
+	const mainElement = React.cloneElement(children, {
 		onMouseUp: handleMouseUp,
 		onMouseDown: handleMouseDown,
 		className: [
-			children.props.className,
+			children.props.className, 'dv-box',
 			isDragging ? 'dv-box-dragging' : null,
-		].join(' '),
+			isResizing ? 'dv-box-resizing' : null,
+		].join(' ').trim(),
 		style: {
 			...children.props.style, 
-			...elementStyle
+			...setElementStyle(position, height, isDragging, isResizing),
 		},
 		children: [
 			children.props.children,
@@ -190,6 +184,21 @@ const Element = ({
 			/>
 		]
 	});
+
+	return (
+		<>
+			{(isDragging || isResizing) ? (
+				cloneElement
+			) : null}
+			{mainElement}
+			{isDragging ? (
+				<aside 
+					className="resize-overlay hide-resize-overlay" 
+					style={{ backgroundColor: 'transparent' }} 
+				/>
+			) : null}
+		</>
+	);
 };
 
 export default Element;
